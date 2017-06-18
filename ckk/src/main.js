f36d30970e4ef221d22aa28f9bc8812264edd18f
Kuan-Wu Chung <521Chung@gmail.com>
2017-06-18 13:17:36 +0800

    Show Tainan data & add vue-awesome

--- ckk/src/main.js
+++ ckk/src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import VueResource from 'vue-resource'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.use(VueResource)
+Vue.component('icon', Icon)
 
 /* eslint-disable no-new */
 new Vue({
