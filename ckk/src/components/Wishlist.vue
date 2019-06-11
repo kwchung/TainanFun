becc499ee5eec32cfebdc85d86267b1ac13cf8df
Kuan-Wu, Chung <kwanwu.chung@gmail.com>
2019-06-11 11:33:30 +0800

    fix bug

--- ckk/src/components/Wishlist.vue
+++ ckk/src/components/Wishlist.vue
@@ -1,37 +1,4 @@
 <template>
-  <!-- <div class="wishlist container">
-        <el-row :gutter="20">
-            <el-col :xs="24" :sm="24" :md="9" :lg="9">
-                <el-row>
-                    <el-col :span="24">
-                        <el-input placeholder="請輸入出發地點..." v-model="map.origin"></el-input>
-                    </el-col>
-                    <el-col :span="24">
-                        <el-table :data="tableData" style="width: 100%" empty-text="請前往「景點」頁面新增願望清單">
-                            <el-table-column type="index"></el-table-column>
-                            <el-table-column label="名稱" prop="location.name"></el-table-column>
-                            <el-table-column label="順序">
-                                <template scope="scope">
-                                    <el-button size="mini" type="primary" icon="el-icon-arrow-up" @click="upWishlist(scope.$index)"></el-button>
-                                    <el-button size="mini" type="primary" icon="el-icon-arrow-down" @click="downWishlist(scope.$index)"></el-button>
-                                </template>
-                            </el-table-column>
-                            <el-table-column label="移除">
-                                <template scope="scope">
-                                    <el-button size="small" type="danger" icon="el-icon-minus" @click="deleteFromWishlist(scope.$index)">從願望清單移除</el-button>
-                                </template>
-                            </el-table-column>
-                        </el-table>
-                    </el-col>
-                    <el-col :span="24">
-                        <el-button id="proc" type="success" @click="showMap()" icon="el-icon-position" :disabled="val==0">安排行程</el-button>
-                    </el-col>
-                </el-row>
-            </el-col>
-            <el-col :xs=" 24 " :sm="24 " :md="15 " :lg="15 ">
-            </el-col>
-        </el-row>
-  </div>-->
   <div>
     <v-text-field label="出發地點" v-model="map.origin" outline></v-text-field>
     <v-list>
@@ -61,7 +28,9 @@
         </v-list-tile-action>
       </v-list-tile>
     </v-list>
-    <v-btn color="success" @click="showMap()" :disabled="val==0"><v-icon>near_me</v-icon>安排行程</v-btn>
+    <v-btn color="success" @click="showMap()" :disabled="val==0">
+      <v-icon>near_me</v-icon>安排行程
+    </v-btn>
     <iframe
       width="100% "
       height="600"
@@ -110,15 +79,21 @@ export default {
   },
   methods: {
     showMap() {
-      this.map.mapOrigin += "臺南+" + this.map.origin;
+      this.tmapOrigin = "&origin=";
+      this.mapWaypoints = "&waypoints=";
+      this.mapDestination = "&destination=";
+      this.map.mapOrigin += this.map.origin;
+      console.log(this.map.mapOrigin);
       this.tableData.forEach(function(element, index, array) {
         if (index !== this.tableData.length - 1) {
-          this.map.mapWaypoints += "臺南+" + element.location.name + "|";
+          this.map.mapWaypoints += element.location.name + "|";
         }
       }, this);
+      console.log(this.map.mapWaypoints);
       this.map.mapWaypoints = this.map.mapWaypoints.slice(0, -1);
       this.map.mapDestination +=
         "臺南+" + this.tableData[this.tableData.length - 1].location.name;
+      console.log(this.map.mapDestination);
       this.map.show = true;
     },
     upWishlist(_index) {
